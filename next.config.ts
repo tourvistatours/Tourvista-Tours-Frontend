1ffcc74e1b18d37698613ebc9bc64e74fc7c2689
LakinduVirajith <86775461+LakinduVirajith@users.noreply.github.com>
2026-05-05 14:17:13 +0530

    testing 1.0.2

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
       },
     ],
   },
